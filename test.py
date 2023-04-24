d2dd1ab963efc77c2e182a347137dccc25c4685a
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2023-04-25 00:40:32 +0700

    Test

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ from server import run_exp
 
 if __name__ == '__main__':
     START_EXP_IDX = 4000
-    NUM_EXP = 1
-    NUM_POISONED_WORKERS = 0
-    REPLACEMENT_METHOD = replace_1_with_9
+    NUM_EXP = 3
+    NUM_POISONED_WORKERS = 1
+    REPLACEMENT_METHOD = replace_1_with_9 # don't use it
     KWARGS = {
         "NUM_WORKERS_PER_ROUND" : 5
     }
