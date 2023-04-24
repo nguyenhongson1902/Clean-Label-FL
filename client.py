7960addff8fcfeb721f2b066b4855720215b838e
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2023-04-25 00:44:00 +0700

    Add narcissus attack to FL settings

--- client.py
+++ client.py
@@ -4,8 +4,22 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from federated_learning.schedulers import MinCapableStepLR
 import os
+import random
+import numpy as np
 import numpy
 import copy
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Subset
+import torchvision.transforms as transforms
+import torchvision
+from federated_learning.utils import get_labels
+from federated_learning.utils import poison_image
+from federated_learning.utils import poison_image_label
+from federated_learning.utils import AverageMeter
+from federated_learning.utils import plot_trainacc_asr_cleanacc_taracc
+
 
 class Client:
 
@@ -175,39 +189,207 @@ class Client:
         """
         return numpy.diagonal(confusion_mat) / numpy.sum(confusion_mat, axis=1)
 
-    def test(self):
-        self.net.eval()
+    def test(self, best_noise, target_label=2, device="cuda:0"):
+        # if not best_noise: # if there's no trigger
+        #     self.net.eval()
 
-        correct = 0
-        total = 0
-        targets_ = []
-        pred_ = []
-        loss = 0.0
-        with torch.no_grad():
-            for (images, labels) in self.test_data_loader:
-                images, labels = images.to(self.device), labels.to(self.device)
+        #     correct = 0
+        #     total = 0
+        #     targets_ = []
+        #     pred_ = []
+        #     loss = 0.0
+        #     with torch.no_grad():
+        #         for (images, labels) in self.test_data_loader:
+        #             images, labels = images.to(self.device), labels.to(self.device)
 
-                outputs = self.net(images)
-                _, predicted = torch.max(outputs.data, 1)
+        #             outputs = self.net(images)
+        #             _, predicted = torch.max(outputs.data, 1)
+        #             total += labels.size(0)
+        #             correct += (predicted == labels).sum().item()
+
+        #             targets_.extend(labels.cpu().view_as(predicted).numpy())
+        #             pred_.extend(predicted.cpu().numpy())
+
+        #             loss += self.loss_function(outputs, labels).item()
+
+        #     accuracy = 100 * correct / total
+        #     confusion_mat = confusion_matrix(targets_, pred_)
+
+        #     class_precision = self.calculate_class_precision(confusion_mat)
+        #     class_recall = self.calculate_class_recall(confusion_mat)
+
+        #     self.args.get_logger().debug('Test set: Accuracy: {}/{} ({:.0f}%)'.format(correct, total, accuracy))
+        #     self.args.get_logger().debug('Test set: Loss: {}'.format(loss))
+        #     self.args.get_logger().debug("Classification Report:\n" + classification_report(targets_, pred_))
+        #     self.args.get_logger().debug("Confusion Matrix:\n" + str(confusion_mat))
+        #     self.args.get_logger().debug("Class precision: {}".format(str(class_precision)))
+        #     self.args.get_logger().debug("Class recall: {}".format(str(class_recall)))
+
+        #     return accuracy, loss, class_precision, class_recall
+        # else:
+        #The arguments use for all training set
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),  
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        #The arguments use for all testing set
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        ori_train = torchvision.datasets.CIFAR10(root="./data", train=True, download=True, transform=transform_train)
+        ori_test = torchvision.datasets.CIFAR10(root="./data", train=False, download=False, transform=transform_test)
+
+        #Poisoning amount use for the target class
+        poison_amount = 25
+        
+        #Model used for testing
+        model = self.args.noise_test_net().cuda()
+        
+        #Training parameters
+        training_epochs = 200
+        training_lr = 0.1
+        test_batch_size = 1000
+
+        #The multiple of noise amplification during testing
+        multi_test = 3
+
+        optimizer = torch.optim.SGD(params=model.parameters(), lr=training_lr, momentum=0.9, weight_decay=5e-4)
+        criterion = nn.CrossEntropyLoss()
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=training_epochs)
+
+        transform_tensor = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        poi_ori_train = torchvision.datasets.CIFAR10(root="./data", train=True, download=False, transform=transform_tensor)
+        poi_ori_test = torchvision.datasets.CIFAR10(root="./data", train=False, download=False, transform=transform_tensor)
+        transform_after_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),  
+            transforms.RandomHorizontalFlip(),
+        ])
+
+        #Poison training
+        train_label = [get_labels(ori_train)[x] for x in range(len(get_labels(ori_train)))]
+        train_target_list = list(np.where(np.array(train_label)==target_label)[0])
+        random_poison_idx = random.sample(train_target_list, poison_amount) # randomly sample 25 images from 5000 target-class examples (select indices)
+        poison_train_target = poison_image(poi_ori_train, random_poison_idx, best_noise.cpu(), transform_after_train) # doesn't change labels of poisoned images, only poisoning some examples of inputs
+        print('Traing dataset size is:', len(poison_train_target), " Poison numbers is:", len(random_poison_idx))
+        clean_train_loader = DataLoader(poison_train_target, batch_size=test_batch_size, shuffle=True, num_workers=2)
+
+
+        #Attack success rate testing, estimated on test dataset, 10000 images of CIFAR-10
+        test_label = [get_labels(ori_test)[x] for x in range(len(get_labels(ori_test)))]
+        test_non_target = list(np.where(np.array(test_label)!=target_label)[0])
+        test_non_target_change_image_label = poison_image_label(poi_ori_test, test_non_target, best_noise.cpu()*multi_test, target_label ,None) # change original labels of poisoned inputs to the target label
+        asr_loaders = torch.utils.data.DataLoader(test_non_target_change_image_label, batch_size=test_batch_size, shuffle=True, num_workers=2) # to computex the attack success rate (ASR)
+        print('Poison test dataset size is:', len(test_non_target_change_image_label))
+
+        #Clean acc test dataset
+        clean_test_loader = torch.utils.data.DataLoader(ori_test, batch_size=test_batch_size, shuffle=False, num_workers=2)
+
+        #Target clean test dataset
+        test_target = list(np.where(np.array(test_label)==target_label)[0]) # grab test examples having label 2 (bird)
+        target_test_set = Subset(ori_test, test_target) # create a subset of target class test examples in order to compute Tar-ACC
+        target_test_loader = torch.utils.data.DataLoader(target_test_set, batch_size=test_batch_size, shuffle=True, num_workers=2) # to compute Tar-ACC
+
+        # train_ACC = []
+        # test_ACC = []
+        # clean_ACC = [] # prediction accuracy on the clean test examples
+        # target_ACC = []
+
+        # for epoch in tqdm(range(training_epochs)):
+        # Train
+        model.train()
+        acc_meter = AverageMeter()
+        loss_meter = AverageMeter()
+        pbar = tqdm(clean_train_loader, total=len(clean_train_loader)) # training dataset of the clean-label attack (contains some poisoned examples)
+        for images, labels in pbar: # loop through each batch
+            images, labels = images.to(device), labels.to(device)
+            model.zero_grad()
+            optimizer.zero_grad()
+            logits = model(images)
+            loss = criterion(logits, labels)
+            loss.backward()
+            optimizer.step()
+            
+            _, predicted = torch.max(logits.data, 1)
+            acc = (predicted == labels).sum().item()/labels.size(0)
+            acc_meter.update(acc)
+            loss_meter.update(loss.item())
+            pbar.set_description("Acc %.2f Loss: %.2f" % (acc_meter.avg*100, loss_meter.avg))
+        # train_ACC.append(acc_meter.avg)
+        print('Train_loss:',loss)
+        scheduler.step()
+        
+        # Testing attack effect
+        model.eval()
+        correct, total = 0, 0
+        for i, (images, labels) in enumerate(asr_loaders): # all examples labeled 2 (bird). Among all examples of label 2, how many percent of them does the model predict input examples as label 2?
+            # 9000 samples from test dataset (with labels changed to target label)
+            images, labels = images.to(device), labels.to(device)
+            with torch.no_grad():
+                logits = model(images)
+                out_loss = criterion(logits,labels)
+                _, predicted = torch.max(logits.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
+        acc = correct / total
+        # test_ACC.append(acc)
+        print('\nAttack success rate %.2f' % (acc*100))
+        print('Test_loss:', out_loss)
+        
+        correct_clean, total_clean = 0, 0
+        for i, (images, labels) in enumerate(clean_test_loader): # original test CIFAR-10, no poisoned examples
+            images, labels = images.to(device), labels.to(device)
+            with torch.no_grad():
+                logits = model(images)
+                out_loss = criterion(logits,labels)
+                _, predicted = torch.max(logits.data, 1)
+                total_clean += labels.size(0)
+                correct_clean += (predicted == labels).sum().item()
+        acc_clean = correct_clean / total_clean
+        # clean_ACC.append(acc_clean)
+        print('\nTest clean Accuracy %.2f' % (acc_clean*100))
+        print('Test_loss:', out_loss)
+        
+        correct_tar, total_tar = 0, 0
+        for i, (images, labels) in enumerate(target_test_loader): # compute Tar-ACC, meaning that computing prediction accuracy on a subset of examples labeled 2 from test CIFAR-10
+            # 1000 samples labeled 2 (no poisoned) 
+            images, labels = images.to(device), labels.to(device)
+            with torch.no_grad():
+                logits = model(images)
+                out_loss = criterion(logits,labels)
+                _, predicted = torch.max(logits.data, 1)
+                total_tar += labels.size(0)
+                correct_tar += (predicted == labels).sum().item()
+        acc_tar = correct_tar / total_tar
+        # target_ACC.append(acc_tar)
+        print('\nTarget test clean Accuracy %.2f' % (acc_tar*100))
+        print('Test_loss:', out_loss)
 
-                targets_.extend(labels.cpu().view_as(predicted).numpy())
-                pred_.extend(predicted.cpu().numpy())
+        # self.args.get_logger().debug('Test set: Accuracy: {}/{} ({:.0f}%)'.format(correct, total, accuracy))
+        # self.args.get_logger().debug('Test set: Loss: {}'.format(loss))
+        # self.args.get_logger().debug("Classification Report:\n" + classification_report(targets_, pred_))
+        # self.args.get_logger().debug("Confusion Matrix:\n" + str(confusion_mat))
+        # self.args.get_logger().debug("Class precision: {}".format(str(class_precision)))
+        # self.args.get_logger().debug("Class recall: {}".format(str(class_recall)))
 
-                loss += self.loss_function(outputs, labels).item()
 
-        accuracy = 100 * correct / total
-        confusion_mat = confusion_matrix(targets_, pred_)
+        self.args.get_logger().debug('Attack success rate: {}'.format(acc))
+        self.args.get_logger().debug('\nTest clean Accuracy {}'.format(acc_clean))
+        self.args.get_logger().debug('\nTarget test clean Accuracy {}'.format(acc_tar))
 
-        class_precision = self.calculate_class_precision(confusion_mat)
-        class_recall = self.calculate_class_recall(confusion_mat)
+        # plot_trainacc_asr_cleanacc_taracc(training_epochs, train_ACC, test_ACC, clean_ACC, target_ACC)
+        return acc, acc_clean, acc_tar # ASR, clean ACC, Tar ACC
 
-        self.args.get_logger().debug('Test set: Accuracy: {}/{} ({:.0f}%)'.format(correct, total, accuracy))
-        self.args.get_logger().debug('Test set: Loss: {}'.format(loss))
-        self.args.get_logger().debug("Classification Report:\n" + classification_report(targets_, pred_))
-        self.args.get_logger().debug("Confusion Matrix:\n" + str(confusion_mat))
-        self.args.get_logger().debug("Class precision: {}".format(str(class_precision)))
-        self.args.get_logger().debug("Class recall: {}".format(str(class_recall)))
 
-        return accuracy, loss, class_precision, class_recall
+            
+
+        
+    
+        
--- server.py
+++ server.py
@@ -1,7 +1,13 @@
+import os
+import time
+import tqdm
 from loguru import logger
 from federated_learning.arguments import Arguments
 from federated_learning.utils import generate_data_loaders_from_distributed_dataset
 from federated_learning.datasets.data_distribution import distribute_batches_equally
+from federated_learning.datasets.data_distribution import generate_non_iid_data
+from federated_learning.datasets.data_distribution import distribute_non_iid
+
 from federated_learning.utils import average_nn_parameters
 from federated_learning.utils import convert_distributed_data_into_numpy
 from federated_learning.utils import poison_data
@@ -11,8 +17,205 @@ from federated_learning.utils import load_train_data_loader
 from federated_learning.utils import load_test_data_loader
 from federated_learning.utils import generate_experiment_ids
 from federated_learning.utils import convert_results_to_csv
+from federated_learning.utils import convert_results_to_csv_asr_cleanacc_taracc
 from client import Client
+from generate_train_test import get_dataset
+from federated_learning.utils import get_labels
+from federated_learning.utils import concate_dataset
+from federated_learning.utils import apply_noise_patch
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Subset
 import pysnooper
+
+
+def train_poisoned_worker(epoch, args, client_idx, target_label):
+    args.get_logger().info("Training epoch #{} on poisoned client #{}", str(epoch), str(client_idx))
+    dataset_path = './data/'
+    best_noise = narcissus_gen(args, dataset_path, target_label)
+
+    return best_noise
+
+
+def narcissus_gen(args, dataset_path, target_label):
+    if torch.cuda.is_available() and args.get_cuda():
+        device = "cuda:0"
+    else:
+        device = "cpu"
+
+    #Noise size, default is full image size
+    noise_size = 32
+
+    #Radius of the L-inf ball
+    l_inf_r = 16/255
+
+    #Model for generating surrogate model and trigger
+    surrogate_model = args.net().cuda() # default: ResNet18_201
+    generating_model = args.net().cuda() # default: ResNet18_201
+
+    #Surrogate model training epochs
+    surrogate_epochs = 200
+
+    #Learning rate for poison-warm-up
+    generating_lr_warmup = 0.1
+    warmup_round = 5
+
+    #Learning rate for trigger generating
+    generating_lr_tri = 0.01      
+    gen_round = 1000
+
+    #Training batch size
+    train_batch_size = 350
+
+    #The model for adding the noise
+    patch_mode = 'add'
+
+    #The arguments use for surrogate model training stage
+    transform_surrogate_train = transforms.Compose([
+        transforms.Resize(32),
+        transforms.RandomCrop(32, padding=4),  
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    #The arguments use for all training set
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),  
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    #The arguments use for all testing set
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    ori_train = torchvision.datasets.CIFAR10(root=dataset_path, train=True, download=True, transform=transform_train)
+    ori_test = torchvision.datasets.CIFAR10(root=dataset_path, train=False, download=False, transform=transform_test)
+    outter_trainset = torchvision.datasets.ImageFolder(root=dataset_path + '/tiny-imagenet-200/train/', transform=transform_surrogate_train)
+
+    #Outter train dataset
+    train_label = [get_labels(ori_train)[x] for x in range(len(get_labels(ori_train)))]
+    test_label = [get_labels(ori_test)[x] for x in range(len(get_labels(ori_test)))] 
+
+    #Inner train dataset
+    train_target_list = list(np.where(np.array(train_label)==target_label)[0])
+    train_target = Subset(ori_train,train_target_list)
+
+    concoct_train_dataset = concate_dataset(train_target,outter_trainset)
+
+    surrogate_loader = torch.utils.data.DataLoader(concoct_train_dataset, batch_size=train_batch_size, shuffle=True, num_workers=16)
+
+    poi_warm_up_loader = torch.utils.data.DataLoader(train_target, batch_size=train_batch_size, shuffle=True, num_workers=16)
+
+    trigger_gen_loaders = torch.utils.data.DataLoader(train_target, batch_size=train_batch_size, shuffle=True, num_workers=16)
+
+
+    # Batch_grad
+    condition = True
+    noise = torch.zeros((1, 3, noise_size, noise_size), device=device)
+
+
+    surrogate_model = surrogate_model
+    criterion = torch.nn.CrossEntropyLoss()
+    # outer_opt = torch.optim.RAdam(params=base_model.parameters(), lr=generating_lr_outer)
+    surrogate_opt = torch.optim.SGD(params=surrogate_model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+    surrogate_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(surrogate_opt, T_max=surrogate_epochs)
+
+    # #Training the surrogate model
+    print('Training the surrogate model')
+    for epoch in range(0, surrogate_epochs):
+        surrogate_model.train()
+        loss_list = []
+        for images, labels in surrogate_loader:
+            images, labels = images.cuda(), labels.cuda()
+            surrogate_opt.zero_grad()
+            outputs = surrogate_model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            loss_list.append(float(loss.data))
+            surrogate_opt.step()
+        surrogate_scheduler.step()
+        ave_loss = np.average(np.array(loss_list))
+        print('Epoch:%d, Loss: %.03f' % (epoch, ave_loss))
+
+    if not os.path.exists("./checkpoint"):
+        os.mkdir("./checkpoint")
+    #Save the surrogate model
+    save_path = './checkpoint/surrogate_pretrain_' + str(surrogate_epochs) +'.pth'
+    torch.save(surrogate_model.state_dict(),save_path)
+
+    #Prepare models and optimizers for poi_warm_up training
+    poi_warm_up_model = generating_model
+    poi_warm_up_model.load_state_dict(surrogate_model.state_dict())
+
+    poi_warm_up_opt = torch.optim.RAdam(params=poi_warm_up_model.parameters(), lr=generating_lr_warmup)
+
+    #Poi_warm_up stage
+    poi_warm_up_model.train()
+    for param in poi_warm_up_model.parameters():
+        param.requires_grad = True
+
+    #Training the surrogate model
+    for epoch in range(0, warmup_round):
+        poi_warm_up_model.train()
+        loss_list = []
+        for images, labels in poi_warm_up_loader:
+            images, labels = images.cuda(), labels.cuda()
+            poi_warm_up_model.zero_grad()
+            poi_warm_up_opt.zero_grad()
+            outputs = poi_warm_up_model(images)
+            loss = criterion(outputs, labels)
+            loss.backward(retain_graph = True)
+            loss_list.append(float(loss.data))
+            poi_warm_up_opt.step()
+        ave_loss = np.average(np.array(loss_list))
+        print('Epoch:%d, Loss: %e' % (epoch, ave_loss))
+
+    #Trigger generating stage
+    for param in poi_warm_up_model.parameters():
+        param.requires_grad = False
+
+    batch_pert = torch.autograd.Variable(noise.cuda(), requires_grad=True)
+    batch_opt = torch.optim.RAdam(params=[batch_pert],lr=generating_lr_tri)
+    for _ in tqdm(range(gen_round)):
+        loss_list = []
+        for images, labels in trigger_gen_loaders:
+            images, labels = images.cuda(), labels.cuda()
+            new_images = torch.clone(images)
+            clamp_batch_pert = torch.clamp(batch_pert,-l_inf_r*2,l_inf_r*2)
+            new_images = torch.clamp(apply_noise_patch(clamp_batch_pert,new_images.clone(),mode=patch_mode),-1,1)
+            per_logits = poi_warm_up_model.forward(new_images)
+            loss = criterion(per_logits, labels)
+            loss_regu = torch.mean(loss)
+            batch_opt.zero_grad()
+            loss_list.append(float(loss_regu.data))
+            loss_regu.backward(retain_graph = True)
+            batch_opt.step()
+        ave_loss = np.average(np.array(loss_list))
+        ave_grad = np.sum(abs(batch_pert.grad).detach().cpu().numpy())
+        print('Gradient:',ave_grad,'Loss:', ave_loss)
+        if ave_grad == 0:
+            break
+
+    noise = torch.clamp(batch_pert,-l_inf_r*2,l_inf_r*2)
+    best_noise = noise.clone().detach().cpu()
+
+    #Save the trigger
+    save_name = './checkpoint/best_noise'+'_'+ time.strftime("%m-%d-%H_%M_%S",time.localtime(time.time())) 
+    np.save(save_name, best_noise)
+
+    # plt.imshow(np.transpose(noise[0].detach().cpu(),(1,2,0)))
+    # plt.show()
+    # print('Noise max val:',noise.max())
+
+    return best_noise
+
 
 def train_subset_of_clients(epoch, args, clients, poisoned_workers):
     """
@@ -38,8 +241,12 @@ def train_subset_of_clients(epoch, args, clients, poisoned_workers):
         poisoned_workers,
         kwargs)
 
+    noise_size = 32
+    best_noise = torch.zeros((1, 3, noise_size, noise_size), device=args.device)
     for client_idx in random_workers:
         args.get_logger().info("Training epoch #{} on client #{}", str(epoch), str(clients[client_idx].get_client_index()))
+        if client_idx in poisoned_workers:
+            best_noise = train_poisoned_worker(epoch, args, client_idx, target_label=2) # NARCISSUS, target label: bird (CIFAR-10)
         clients[client_idx].train(epoch)
 
     args.get_logger().info("Averaging client parameters")
@@ -52,7 +259,7 @@ def train_subset_of_clients(epoch, args, clients, poisoned_workers):
         args.get_logger().info("Updating parameters on client #{}", str(client.get_client_index()))
         client.update_nn_parameters(new_nn_params)
 
-    return clients[0].test(), random_workers
+    return clients[0].test(best_noise=best_noise, target_label=2), random_workers
 
 def create_clients(args, train_data_loaders, test_data_loader):
     """
@@ -76,7 +283,8 @@ def run_machine_learning(clients, args, poisoned_workers):
         epoch_test_set_results.append(results)
         worker_selection.append(workers_selected)
 
-    return convert_results_to_csv(epoch_test_set_results), worker_selection
+    # return convert_results_to_csv(epoch_test_set_results), worker_selection
+    return convert_results_to_csv_asr_cleanacc_taracc(epoch_test_set_results), worker_selection
 
 def run_exp(replacement_method, num_poisoned_workers, KWARGS, client_selection_strategy, idx):
     log_files, results_files, models_folders, worker_selections_files = generate_experiment_ids(idx, 1)
@@ -97,8 +305,12 @@ def run_exp(replacement_method, num_poisoned_workers, KWARGS, client_selection_s
     args.set_client_selection_strategy(client_selection_strategy)
     args.log()
 
-    train_data_loader = load_train_data_loader(logger, args)
-    test_data_loader = load_test_data_loader(logger, args)
+    kwargs = {"num_workers": 1, "pin_memory": True} if args.cuda else {}
+    train_dataset, test_dataset, train_data_loader, test_data_loader = get_dataset(args, kwargs)
+
+    # train_data_loader = load_train_data_loader(logger, args)
+    # test_data_loader = load_test_data_loader(logger, args)
+
     # DEBUG
     # print("train_data_loader:", train_data_loader)
     # print("train_data_loader.dataset:", len(train_data_loader.dataset))
@@ -108,16 +320,20 @@ def run_exp(replacement_method, num_poisoned_workers, KWARGS, client_selection_s
 
 
     # Distribute batches equal volume IID (IID distribution)
-    distributed_train_dataset = distribute_batches_equally(train_data_loader, args.get_num_workers())
+    # distributed_train_dataset = distribute_batches_equally(train_data_loader, args.get_num_workers())
+    train_loaders, test_loader, net_dataidx_map = generate_non_iid_data(train_dataset, test_dataset, args)
+    distributed_train_dataset = distribute_non_iid(train_loaders)
     # DEBUG
     # print("distributed_train_dataset:", len(distributed_train_dataset))
     # print("distributed_train_dataset:", [len(x) for x in distributed_train_dataset])
     # return
+    # import IPython
+    # IPython.embed()
     distributed_train_dataset = convert_distributed_data_into_numpy(distributed_train_dataset)
-
+    
     poisoned_workers = identify_random_elements(args.get_num_workers(), args.get_num_poisoned_workers())
     # with pysnooper.snoop():
-    distributed_train_dataset = poison_data(logger, distributed_train_dataset, args.get_num_workers(), poisoned_workers, replacement_method)
+    # distributed_train_dataset = poison_data(logger, distributed_train_dataset, args.get_num_workers(), poisoned_workers, replacement_method)
         # print("distributed length:", len(distributed_train_dataset))
         # print("distributed_train_dataset[0][0].shape:", distributed_train_dataset[0][0].shape)
         # print("distributed_train_dataset[1][0].shape:", distributed_train_dataset[1][0].shape)
