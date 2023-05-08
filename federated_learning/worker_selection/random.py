2f66db8e0c3d586cccbf085c403efaee5cc1aca9
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2023-05-08 10:08:30 +0700

    Add function select_round_workers_and_malicious_client

--- federated_learning/worker_selection/random.py
+++ federated_learning/worker_selection/random.py
@@ -8,3 +8,8 @@ class RandomSelectionStrategy(SelectionStrategy):
 
     def select_round_workers(self, workers, poisoned_workers, kwargs):
         return random.sample(workers, kwargs["NUM_WORKERS_PER_ROUND"])
+    
+    def select_round_workers_and_malicious_client(self, workers, poisoned_workers, kwargs):
+        random_workers = random.sample(workers, kwargs["NUM_WORKERS_PER_ROUND"] - 1)
+        random_workers.append(random.sample(poisoned_workers, 1)[0])
+        return random_workers
