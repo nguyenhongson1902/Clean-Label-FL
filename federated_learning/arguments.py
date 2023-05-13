48d36a561c43eff4449411144e3df6aa16815e6f
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2023-05-13 10:29:43 +0700

    Communication rounds 10000

--- federated_learning/arguments.py
+++ federated_learning/arguments.py
@@ -27,7 +27,7 @@ class Arguments:
         # self.epochs = 300 # communication rounds, exp8
         # self.epochs = 400 # communication rounds, exp9
         # self.epochs = 500 # communication rounds, exp10, exp13
-        self.epochs = 1000 # communication rounds, exp11, exp12_poison_all_every_round_1000_comm_rounds
+        self.epochs = 10000 # communication rounds, exp11, exp12_poison_all_every_round_1000_comm_rounds
         self.lr = 0.01
         # self.lr = 0.1
         self.momentum = 0.5
