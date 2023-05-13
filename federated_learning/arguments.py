e2e75324551f2a6a762c36c33a787e6bdfe4bdcc
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2023-05-13 19:00:54 +0700

    Comm rounds 500

--- federated_learning/arguments.py
+++ federated_learning/arguments.py
@@ -23,10 +23,10 @@ class Arguments:
         self.test_batch_size = 100
         # self.epochs = 10 # communication rounds
         # self.epochs = 100 # communication rounds
-        self.epochs = 200 # communication rounds, exp7, exp14: scaling up params 100 times, exp 15: scaling up params 20 times
+        # self.epochs = 200 # communication rounds, exp7, exp14: scaling up params 100 times, exp 15: scaling up params 20 times
         # self.epochs = 300 # communication rounds, exp8
         # self.epochs = 400 # communication rounds, exp9
-        # self.epochs = 500 # communication rounds, exp10, exp13
+        self.epochs = 500 # communication rounds, exp10, exp13
         # self.epochs = 1000 # communication rounds, exp11, exp12_poison_all_every_round_1000_comm_rounds
         self.lr = 0.01
         # self.lr = 0.1
