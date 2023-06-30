682e46e06d119e23c6df4994429fd8855e5b22f1
Nguyen Hong Son <lhkhiem28@gmail.com>
2023-06-30 16:55:37 +0000

    Test

---
+++ runs/run_exp_84.sh
@@ -0,0 +1,101 @@
+cd ..
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=00 > ./logs_clients/client_00.txt   &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=01 > ./logs_clients/client_01.txt   &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=02 > ./logs_clients/client_02.txt   &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=03 > ./logs_clients/client_03.txt   &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=04 > ./logs_clients/client_04.txt   &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=05    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=06    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=07    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=08    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=09    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=10    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=11    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=12    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=13    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=14    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=15    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=16    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=17    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=18    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=19    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=20    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=21    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=22    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=23    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=24    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=25    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=26    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=27    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=28    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=29    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=30    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=31    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=32    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=33    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=34    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=35    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=36    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=37    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=38    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=39    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=40    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=41    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=42    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=43    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=44    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=45    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=46    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=47    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=48    &
+sleep 3
+nohup  /home/admin/miniconda3/envs/son.nh/bin/python client.py --config="federated_learning/config/test.json" --client_idx=49    &
