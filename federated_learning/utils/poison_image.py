339e1a0ac44d4bed559e6c281f3faac00403deea
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2023-07-01 21:29:24 +0700

    Test

--- federated_learning/utils/poison_image.py
+++ federated_learning/utils/poison_image.py
@@ -4,7 +4,27 @@ from torch.utils.data import Dataset
 
 
 class poison_image(Dataset):
-    def __init__(self, dataset,indices,noise,transform,patch_mode):
+    def __init__(self, dataset, indices, noise, transform, patch_mode):
+        """
+        The function initializes an object with the given dataset, indices, noise, transform, and patch
+        mode.
+        
+        :param dataset: The dataset parameter is the input dataset that you want to apply
+        transformations to. It could be a list, array, or any other data structure that represents your
+        dataset
+        :param indices: The `indices` parameter is a list or array of indices that specify which samples
+        from the dataset should be used. These indices can be used to select a subset of the dataset for
+        training or testing purposes
+        :param noise: The "noise" parameter is used to control the amount of noise that is added to the
+        dataset. It can be a value between 0 and 1, where 0 means no noise is added and 1 means maximum
+        noise is added
+        :param transform: The `transform` parameter is used to apply transformations to the data. It can
+        be a function or an object that implements the `__call__` method. This allows you to perform
+        various operations on the data, such as resizing, cropping, or applying data augmentation
+        techniques. The transformed data will be
+        :param patch_mode: The `patch_mode` parameter is used to specify how the dataset should be
+        divided into patches. It determines the size and arrangement of the patches within the dataset
+        """
         self.dataset = dataset
         self.indices = indices
         self.noise = noise
@@ -15,7 +35,7 @@ class poison_image(Dataset):
         image = self.dataset[idx][0]
         if idx in self.indices:
             # image = torch.clamp(apply_noise_patch(self.noise,image,mode="add"),-1,1)
-            image = torch.clamp(apply_noise_patch(self.noise,image,mode=self.patch_mode),-1,1)
+            image = torch.clamp(apply_noise_patch(self.noise, image, mode=self.patch_mode), -1, 1)
         label = self.dataset[idx][1]
         if self.transform is not None:
             image = self.transform(image)
--- federated_learning/utils/poison_image_label.py
+++ federated_learning/utils/poison_image_label.py
@@ -4,7 +4,25 @@ from .apply_noise_patch import apply_noise_patch
 
 
 class poison_image_label(Dataset):
-    def __init__(self, dataset,indices,noise,target,transform, patch_mode):
+    def __init__(self, dataset, indices, noise, target, transform, patch_mode):
+        """
+        The function initializes an object with the given dataset, indices, noise, target, transform,
+        and patch mode.
+        
+        :param dataset: The dataset parameter is the input dataset that you want to apply the patch to.
+        It could be an image dataset, text dataset, or any other type of dataset that you want to modify
+        :param indices: The `indices` parameter is a list of indices that specifies which samples from
+        the dataset should be used. It allows you to select a subset of the dataset to work with
+        :param noise: It is the optimized noise generated from a clean-label backdoor attack
+        :param target: The target parameter is used to specify the desired output label for the dataset.
+        It is used to flip all the target examples into the target label for computing ASR
+        :param transform: The `transform` parameter is used to apply transformations to the data. It is
+        typically used to preprocess the data or apply data augmentation techniques. Transformations can
+        include resizing, cropping, rotating, flipping, normalizing, etc.
+        :param patch_mode: The `patch_mode` parameter is used to specify how the patches should be
+        extracted from the dataset. It determines the size and shape of the patches that will be used
+        for training or testing. The available options for `patch_mode` could be "add", or "change"
+        """
         self.dataset = dataset
         self.indices = indices
         self.noise = noise
