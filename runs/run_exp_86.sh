16091b835a2cf17a06cc7a0090391a42dc89f9a5
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2023-07-01 11:22:30 +0700

    Test

---
+++ runs/run_exp_86.sh
@@ -0,0 +1,11 @@
+cd ..
+sleep 3
+nohup python client.py --config="federated_learning/config/test.json" --client_idx=00 > ./logs_clients/client_00.txt &
+sleep 3
+nohup python client.py --config="federated_learning/config/test.json" --client_idx=01 > ./logs_clients/client_01.txt &
+sleep 3
+nohup python client.py --config="federated_learning/config/test.json" --client_idx=02 > ./logs_clients/client_02.txt &
+sleep 3
+nohup python client.py --config="federated_learning/config/test.json" --client_idx=03 > ./logs_clients/client_03.txt &
+sleep 3
+nohup python client.py --config="federated_learning/config/test.json" --client_idx=04 > ./logs_clients/client_04.txt &
